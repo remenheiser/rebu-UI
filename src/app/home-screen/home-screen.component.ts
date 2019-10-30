f24a772e6e93c11341e6dff08b45ad2ba082c86c
Korey Mitchell <kmitchel@udel.edu>
2019-10-30 13:18:18 -0400

    Updates to home

--- src/app/home-screen/home-screen.component.ts
+++ src/app/home-screen/home-screen.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home-screen',
   templateUrl: './home-screen.component.html',
-  styleUrls: ['./home-screen.component.css']
+  styleUrls: ['./home-screen.component.scss']
 })
 export class HomeScreenComponent implements OnInit {
 
