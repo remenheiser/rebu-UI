72367298af698059e192d0362be85b1e8d5a7267
Marwane <marwane@udel.edu>
2019-11-01 14:57:13 -0400

    fixed signup toggle

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from "@angular/core";
-
 @Component({
   selector: "app-signup",
   templateUrl: "./signup.component.html",
@@ -9,22 +8,23 @@ export class SignupComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
+
+  toggleSignup() {
+    document.getElementById("login-toggle").style.backgroundColor = "#fff";
+    document.getElementById("login-toggle").style.color = "#222";
+    document.getElementById("signup-toggle").style.backgroundColor = "#e64a19";
+    document.getElementById("signup-toggle").style.color = "#fff";
+    document.getElementById("login-form").style.display = "none";
+    document.getElementById("signup-form").style.display = "block";
+  }
+
+  toggleLogin() {
+    document.getElementById("login-toggle").style.backgroundColor = "#e64a19";
+    document.getElementById("login-toggle").style.color = "#fff";
+    document.getElementById("signup-toggle").style.backgroundColor = "#fff";
+    document.getElementById("signup-toggle").style.color = "#222";
+    document.getElementById("signup-form").style.display = "none";
+    document.getElementById("login-form").style.display = "block";
+  }
 }
 //------------this is the JS associated w the signup
-function toggleSignup() {
-  document.getElementById("login-toggle").style.backgroundColor = "#fff";
-  document.getElementById("login-toggle").style.color = "#222";
-  document.getElementById("signup-toggle").style.backgroundColor = "#57b846";
-  document.getElementById("signup-toggle").style.color = "#fff";
-  document.getElementById("login-form").style.display = "none";
-  document.getElementById("signup-form").style.display = "block";
-}
-
-function toggleLogin() {
-  document.getElementById("login-toggle").style.backgroundColor = "#57B846";
-  document.getElementById("login-toggle").style.color = "#fff";
-  document.getElementById("signup-toggle").style.backgroundColor = "#fff";
-  document.getElementById("signup-toggle").style.color = "#222";
-  document.getElementById("signup-form").style.display = "none";
-  document.getElementById("login-form").style.display = "block";
-}
