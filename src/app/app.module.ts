07f3348b719f1bae2e1a3fb610c1ce53c3950995
Marwane <marwane@udel.edu>
2019-11-08 13:48:09 -0500

    paypal UP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HTTP_INTERCEPTORS} from '@angular/common/http';
-import { AppRoutingModule, routingComponents } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { SpotsComponent } from './spots/spots.component';
-import { HttpClientModule } from '@angular/common/http';
-import { ChosenSpotComponent } from './chosen-spot/chosen-spot.component';
-import { HomeScreenComponent } from './home-screen/home-screen.component';
-import { AboutComponent } from './about/about.component';
-import { AgmCoreModule } from '@agm/core';
-import { ListComponent } from './list/list.component';
-import { NearByComponent } from './near-by/near-by.component';
-import * as $ from 'jquery';
-import { LoginComponent } from './login/login.component';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import { AgmDirectionModule } from 'agm-direction';
-import { SignupComponent } from './signup/signup.component';
-import { PostlistComponent } from './postlist/postlist.component';
-import { CustomHttpInterceptorService } from './httpinterceptor';
-import { UnauthorizedComponent } from './unauthorized/unauthorized.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HTTP_INTERCEPTORS } from "@angular/common/http";
+import { AppRoutingModule, routingComponents } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { SpotsComponent } from "./spots/spots.component";
+import { HttpClientModule } from "@angular/common/http";
+import { ChosenSpotComponent } from "./chosen-spot/chosen-spot.component";
+import { HomeScreenComponent } from "./home-screen/home-screen.component";
+import { AboutComponent } from "./about/about.component";
+import { AgmCoreModule } from "@agm/core";
+import { ListComponent } from "./list/list.component";
+import { NearByComponent } from "./near-by/near-by.component";
+import * as $ from "jquery";
+import { LoginComponent } from "./login/login.component";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { AgmDirectionModule } from "agm-direction";
+import { SignupComponent } from "./signup/signup.component";
+import { PostlistComponent } from "./postlist/postlist.component";
+import { CustomHttpInterceptorService } from "./httpinterceptor";
+import { UnauthorizedComponent } from "./unauthorized/unauthorized.component";
+import { NgxPayPalModule } from "ngx-paypal";
 
 @NgModule({
   declarations: [
@@ -37,17 +38,22 @@ import { UnauthorizedComponent } from './unauthorized/unauthorized.component';
   ],
   imports: [
     BrowserModule,
-      NgbModule,
-      AgmDirectionModule,
+    NgbModule,
+    AgmDirectionModule,
     AppRoutingModule,
     HttpClientModule,
     AgmCoreModule.forRoot({
-      apiKey: 'AIzaSyB5OJt9-wsH0qPuYPpTny8IlEruNUufNwI'
+      apiKey: "AIzaSyB5OJt9-wsH0qPuYPpTny8IlEruNUufNwI"
     }),
+    NgxPayPalModule
   ],
-  providers: [ 
-    {provide: HTTP_INTERCEPTORS, useClass: CustomHttpInterceptorService, multi: true},
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: CustomHttpInterceptorService,
+      multi: true
+    }
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/chosen-spot/chosen-spot.component.ts
+++ src/app/chosen-spot/chosen-spot.component.ts
@@ -1,181 +1,234 @@
 /// <reference types="@types/googlemaps" />
-import { Component, OnInit } from '@angular/core';
-import { ChosenSpotService } from './chosen-spot.service';
-import { ActivatedRoute, Router, ParamMap } from '@angular/router'
-import { GoogleMapsAPIWrapper, MapsAPILoader } from '@agm/core';
-import { Observable } from 'rxjs';
+import { Component, OnInit } from "@angular/core";
+import { ChosenSpotService } from "./chosen-spot.service";
+import { ActivatedRoute, Router, ParamMap } from "@angular/router";
+import { GoogleMapsAPIWrapper, MapsAPILoader } from "@agm/core";
+import { Observable } from "rxjs";
+import { IPayPalConfig, ICreateOrderRequest } from "ngx-paypal";
 
 @Component({
-  selector: 'app-chosen-spot',
-  templateUrl: './chosen-spot.component.html',
-  styleUrls: ['./chosen-spot.component.css']
+  selector: "app-chosen-spot",
+  templateUrl: "./chosen-spot.component.html",
+  styleUrls: ["./chosen-spot.component.css"]
 })
-
-
-
-
-
 export class ChosenSpotComponent implements OnInit {
+  public spotsData: any = [];
 
-  public spotsData: any = []
-
-
-  constructor(private _chosenSpotService: ChosenSpotService, private route: ActivatedRoute, private router: Router) { }
+  constructor(
+    private _chosenSpotService: ChosenSpotService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   selectId: number;
-
 
   public lat = 0;
   public lng = 0;
 
-
   public origin: any;
   public destination: any;
 
-
+  public payPalConfig?: IPayPalConfig;
 
   ngOnInit() {
+    this.initConfig();
+    setTimeout(this.startMap, 2000);
 
-    function startMap() {
+    // var map = new google.maps.Map(document.getElementById('map'), {
+    //       zoom: 7,
+    //       center: {lat: 41.85, lng: -87.65}
+    //     });
+    // var infoWindow;
+    // this.origin = { lat: 39.6837, lng: -75.7497 };
+    // this.destination = { lat: 39.7447, lng: -75.5484 };
 
+    //   function getLatLng() {
+    //   var user1Location = $("#mainTitle").text();
+    //   var geocoder = new google.maps.Geocoder();
+    //   //convert location into longitude and latitude
+    //   geocoder.geocode({
+    //       address: user1Location
+    //   }, function(locResult) {
+    //       console.log(locResult);
+    //       var lat1 = locResult[0].geometry.location.lat();
+    //       var lng1 = locResult[0].geometry.location.lng();
+    //       $("#testDiv").html("latitude:" + lat1 + "<p>longitude:" + lng1 + "</p>");
+    //       var pos = {
+    //                   lat: lat1,
+    //                   lng: lng1
+    //                 };
+    //                map.setCenter(new google.maps.LatLng( 39.6837, -75.7497 ) );
+    //                map.destination = new google.maps.LatLng( 39.6837, -75.7497 )
+    //   });
+    // }
 
-      var directionsService = new google.maps.DirectionsService();
-      var directionsRenderer = new google.maps.DirectionsRenderer();
+    // setTimeout(getLatLng, 2000);
 
+    // initMap();
+    // function initMap() {
+    //   var directionsService = new google.maps.DirectionsService;
+    //   var directionsDisplay = new google.maps.DirectionsRenderer;
+    //   var map = new google.maps.Map(document.getElementById('map'), {
+    //     zoom: 7,
+    //     center: {lat: 41.85, lng: -87.65}
+    //   });
 
-      function initMap() {
+    //   infoWindow = new google.maps.InfoWindow;
 
-        var chicago = new google.maps.LatLng(41.850033, -87.6500523);
-        var mapOptions = {
-          zoom: 7,
-          center: chicago
-        }
-        var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-        directionsRenderer.setMap(map);
-      }
+    //     // Try HTML5 geolocation.
+    //     if (navigator.geolocation) {
+    //       navigator.geolocation.getCurrentPosition(function(position) {
+    //         var pos = {
+    //           lat: position.coords.latitude,
+    //           lng: position.coords.longitude
+    //         };
 
-      function calcRoute() {
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function (position) {
-            var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
+    //         infoWindow.setPosition(pos);
+    //         infoWindow.setContent('Location found.');
+    //         infoWindow.open(map);
+    //         map.setCenter(pos);
+    //       })
+    //     }
+    //   }
 
-            var start = new google.maps.LatLng(pos.lat, pos.lng)
-            var end = $('#mainTitle').text();
-            
+    let id = this.route.snapshot.params["id"];
+    this.selectId = id;
 
+    this.spotsData = [];
 
-            $("#phoneButton").click(function(){ 
-              window.window.open("https://www.google.com/maps/dir/"+ pos.lat + ","+ pos.lng +"/" + end, '_blank');
-            });
-
-            var request = {
-              origin: start,
-              destination: end,
-              travelMode: google.maps.TravelMode.DRIVING
-            };
-            directionsService.route(request, function (result, status) {
-              if (status == 'OK') {
-                console.log("OK!");
-                directionsRenderer.setDirections(result);
-              } else {
-                console.log("Status: " + status)
-              }
-            });
-          })
-  
-}
-      }
-        initMap();
-        calcRoute();
-      }
-
-
-      setTimeout(startMap, 2000);
-
-
-
-
-
-
-      // var map = new google.maps.Map(document.getElementById('map'), {
-      //       zoom: 7,
-      //       center: {lat: 41.85, lng: -87.65}
-      //     });
-      // var infoWindow;
-      // this.origin = { lat: 39.6837, lng: -75.7497 };
-      // this.destination = { lat: 39.7447, lng: -75.5484 };
-
-
-
-      //   function getLatLng() {
-      //   var user1Location = $("#mainTitle").text();
-      //   var geocoder = new google.maps.Geocoder();
-      //   //convert location into longitude and latitude
-      //   geocoder.geocode({
-      //       address: user1Location
-      //   }, function(locResult) {
-      //       console.log(locResult);
-      //       var lat1 = locResult[0].geometry.location.lat();
-      //       var lng1 = locResult[0].geometry.location.lng();
-      //       $("#testDiv").html("latitude:" + lat1 + "<p>longitude:" + lng1 + "</p>");
-      //       var pos = {
-      //                   lat: lat1,
-      //                   lng: lng1
-      //                 };
-      //                map.setCenter(new google.maps.LatLng( 39.6837, -75.7497 ) );
-      //                map.destination = new google.maps.LatLng( 39.6837, -75.7497 )
-      //   });
-      // }
-
-      // setTimeout(getLatLng, 2000);
-
-      // initMap(); 
-      // function initMap() {
-      //   var directionsService = new google.maps.DirectionsService;
-      //   var directionsDisplay = new google.maps.DirectionsRenderer;
-      //   var map = new google.maps.Map(document.getElementById('map'), {
-      //     zoom: 7,
-      //     center: {lat: 41.85, lng: -87.65}
-      //   });
-
-      //   infoWindow = new google.maps.InfoWindow;
-
-      //     // Try HTML5 geolocation.
-      //     if (navigator.geolocation) {
-      //       navigator.geolocation.getCurrentPosition(function(position) {
-      //         var pos = {
-      //           lat: position.coords.latitude,
-      //           lng: position.coords.longitude
-      //         };
-
-      //         infoWindow.setPosition(pos);
-      //         infoWindow.setContent('Location found.');
-      //         infoWindow.open(map);
-      //         map.setCenter(pos);
-      //       })
-      //     }
-      //   }
-
-
-      let id = this.route.snapshot.params["id"];
-      this.selectId = id;
-
-
-      this.spotsData = [];
-
-
-
-      this._chosenSpotService.getSpots(this.selectId)
-        .subscribe((data: any) => {
-          console.log(this.spotsData);
-          this.spotsData = data;
+    this._chosenSpotService.getSpots(this.selectId).subscribe((data: any) => {
+      console.log(this.spotsData);
+      this.spotsData = data;
+    });
+  }
+  //
+  initConfig() {
+    this.payPalConfig = {
+      currency: "EUR",
+      clientId: "sb",
+      createOrderOnClient: data =>
+        <ICreateOrderRequest>{
+          intent: "CAPTURE",
+          purchase_units: [
+            {
+              amount: {
+                currency_code: "EUR",
+                value: "9.99",
+                breakdown: {
+                  item_total: {
+                    currency_code: "EUR",
+                    value: "9.99"
+                  }
+                }
+              },
+              items: [
+                {
+                  name: "Enterprise Subscription",
+                  quantity: "1",
+                  category: "DIGITAL_GOODS",
+                  unit_amount: {
+                    currency_code: "EUR",
+                    value: "9.99"
+                  }
+                }
+              ]
+            }
+          ]
+        },
+      advanced: {
+        commit: "true"
+      },
+      style: {
+        label: "paypal",
+        layout: "vertical"
+      },
+      onApprove: (data, actions) => {
+        console.log(
+          "onApprove - transaction was approved, but not authorized",
+          data,
+          actions
+        );
+        actions.order.get().then(details => {
+          console.log(
+            "onApprove - you can get full order details inside onApprove: ",
+            details
+          );
         });
-
-
-
-    }
+      },
+      onClientAuthorization: data => {
+        console.log(
+          "onClientAuthorization - you should probably inform your server about completed transaction at this point",
+          data
+        );
+        //this.showSuccess = true;
+      },
+      onCancel: (data, actions) => {
+        console.log("OnCancel", data, actions);
+      },
+      onError: err => {
+        console.log("OnError", err);
+      },
+      onClick: (data, actions) => {
+        console.log("onClick", data, actions);
+      }
+    };
   }
 
+  //
+  startMap() {
+    var directionsService = new google.maps.DirectionsService();
+    var directionsRenderer = new google.maps.DirectionsRenderer();
+
+    function initMap() {
+      var chicago = new google.maps.LatLng(41.850033, -87.6500523);
+      var mapOptions = {
+        zoom: 7,
+        center: chicago
+      };
+      var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+      directionsRenderer.setMap(map);
+    }
+
+    function calcRoute() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          var pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          };
+
+          var start = new google.maps.LatLng(pos.lat, pos.lng);
+          var end = $("#mainTitle").text();
+
+          $("#phoneButton").click(function() {
+            window.window.open(
+              "https://www.google.com/maps/dir/" +
+                pos.lat +
+                "," +
+                pos.lng +
+                "/" +
+                end,
+              "_blank"
+            );
+          });
+
+          var request = {
+            origin: start,
+            destination: end,
+            travelMode: google.maps.TravelMode.DRIVING
+          };
+          directionsService.route(request, function(result, status) {
+            if (status == "OK") {
+              console.log("OK!");
+              directionsRenderer.setDirections(result);
+            } else {
+              console.log("Status: " + status);
+            }
+          });
+        });
+      }
+    }
+    initMap();
+    calcRoute();
+  }
+}
