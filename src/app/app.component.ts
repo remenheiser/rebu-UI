536f37d5cbb00743f703bc1b588fd6f0e8b3b331
Korey Mitchell <koreymitchell@wifi-roaming-128-4-94-87.host.udel.edu>
2019-12-11 15:49:53 -0500

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,181 +23,181 @@ export class AppComponent {
 
   ngOnInit() {
     //OPEN TRIGGER
-    var openTrigger = $('.menu-trigger');
-    var openTriggerTop = openTrigger.find('.menu-trigger-bar.top');
-    var openTriggerMiddle = openTrigger.find('.menu-trigger-bar.middle');
-    var openTriggerBottom = openTrigger.find('.menu-trigger-bar.bottom');
+    // var openTrigger = $('.menu-trigger');
+    // var openTriggerTop = openTrigger.find('.menu-trigger-bar.top');
+    // var openTriggerMiddle = openTrigger.find('.menu-trigger-bar.middle');
+    // var openTriggerBottom = openTrigger.find('.menu-trigger-bar.bottom');
 
-    //CLOSE TRIGGER
-    var closeTrigger = $('.close-trigger');
-    var closeTrigger1 = $('.close-trigger1');
-    var closeTriggerLeft = closeTrigger.find('.close-trigger-bar.left');
-    var closeTriggerRight = closeTrigger.find('.close-trigger-bar.right');
+    // //CLOSE TRIGGER
+    // var closeTrigger = $('.close-trigger');
+    // var closeTrigger1 = $('.close-trigger1');
+    // var closeTriggerLeft = closeTrigger.find('.close-trigger-bar.left');
+    // var closeTriggerRight = closeTrigger.find('.close-trigger-bar.right');
 
-    //LOGO
-    var logo = $('.logo');
+    // //LOGO
+    // var logo = $('.logo');
 
-    //MENU
-    var menuContainer = $('.menu-container');
-    var menu = $('.menu');
-    var menuTop = $('.menu-bg.top');
-    var menuMiddle = $('.menu-bg.middle');
-    var menuBottom = $('.menu-bg.bottom');
+    // //MENU
+    // var menuContainer = $('.menu-container');
+    // var menu = $('.menu');
+    // var menuTop = $('.menu-bg.top');
+    // var menuMiddle = $('.menu-bg.middle');
+    // var menuBottom = $('.menu-bg.bottom');
 
-    //TL
-    var tlOpen = new TimelineMax({ paused: true });
-    var tlClose = new TimelineMax({ paused: true });
-    this.tlOpen = tlOpen;
-    this.tlClose = tlClose;
+    // //TL
+    // var tlOpen = new TimelineMax({ paused: true });
+    // var tlClose = new TimelineMax({ paused: true });
+    // this.tlOpen = tlOpen;
+    // this.tlClose = tlClose;
 
-    function preventBehavior(e) {
-    e.preventDefault(); 
-    };
+    // function preventBehavior(e) {
+    // e.preventDefault(); 
+    // };
 
-    function preventBehavior1(e) {
+    // function preventBehavior1(e) {
      
-      };
-    //OPEN TIMELINE
-    tlOpen.add("preOpen")
-      .to(logo, 0.4, {
-        scale: 0.8,
-        opacity: 0,
-        ease: Power2.easeOut
-      }, "preOpen")
-      .to(openTriggerTop, 0.4, {
-        x: "+80px", y: "-80px", delay: 0.1, ease: Power4.easeIn, onComplete: function () {
-          // closeTrigger.css('z-index', '25');
-        }
-      }, "preOpen")
-      .to(openTriggerMiddle, 0.4, {
-        x: "+=80px", y: "-=80px", ease: Power4.easeIn,
-        onComplete: function () {
-          openTrigger.css('visibility', 'hidden');
-        }
-      }, "preOpen")
-      .to(openTriggerBottom, 0.4, {
-        x: "+=80px", y: "-=80px", delay: 0.2, ease: Power4.easeIn
-      }, "preOpen")
-      .add("open", "-=0.4")
-      .to(menuTop, 0.8, {
+    //   };
+    // //OPEN TIMELINE
+    // tlOpen.add("preOpen")
+    //   .to(logo, 0.4, {
+    //     scale: 0.8,
+    //     opacity: 0,
+    //     ease: Power2.easeOut
+    //   }, "preOpen")
+    //   .to(openTriggerTop, 0.4, {
+    //     x: "+80px", y: "-80px", delay: 0.1, ease: Power4.easeIn, onComplete: function () {
+    //       // closeTrigger.css('z-index', '25');
+    //     }
+    //   }, "preOpen")
+    //   .to(openTriggerMiddle, 0.4, {
+    //     x: "+=80px", y: "-=80px", ease: Power4.easeIn,
+    //     onComplete: function () {
+    //       openTrigger.css('visibility', 'hidden');
+    //     }
+    //   }, "preOpen")
+    //   .to(openTriggerBottom, 0.4, {
+    //     x: "+=80px", y: "-=80px", delay: 0.2, ease: Power4.easeIn
+    //   }, "preOpen")
+    //   .add("open", "-=0.4")
+    //   .to(menuTop, 0.8, {
         
-        y: "13%",
-        ease: Power4.easeInOut
-      }, "open")
-      .to(menuMiddle, 0.8, {
+    //     y: "13%",
+    //     ease: Power4.easeInOut
+    //   }, "open")
+    //   .to(menuMiddle, 0.8, {
         
-        scaleY: 1,
-        ease: Power4.easeInOut
-      }, "open")
-      .to(menuBottom, 0.8, {
+    //     scaleY: 1,
+    //     ease: Power4.easeInOut
+    //   }, "open")
+    //   .to(menuBottom, 0.8, {
         
-        y: "-114%",
-        ease: Power4.easeInOut
-      }, "open")
-      .fromTo(menu, 0.6, {
-        y: 30, opacity: 0, visibility: 'hidden'
-      }, {
-        y: 0, opacity: 1, visibility: 'visible', ease: Power4.easeOut
-      }, "-=0.2")
-      .add("preClose", "-=0.8")
-      .to(closeTriggerLeft, 0.8, {
-        x: "-=100px", y: "+=100px", ease: Power4.easeOut
-      }, "preClose")
-      .to(closeTriggerRight, 0.8, {
-        x: "+=100px", y: "+=100px", delay: 0.2, ease: Power4.easeOut
-      }, "preClose");
+    //     y: "-114%",
+    //     ease: Power4.easeInOut
+    //   }, "open")
+    //   .fromTo(menu, 0.6, {
+    //     y: 30, opacity: 0, visibility: 'hidden'
+    //   }, {
+    //     y: 0, opacity: 1, visibility: 'visible', ease: Power4.easeOut
+    //   }, "-=0.2")
+    //   .add("preClose", "-=0.8")
+    //   .to(closeTriggerLeft, 0.8, {
+    //     x: "-=100px", y: "+=100px", ease: Power4.easeOut
+    //   }, "preClose")
+    //   .to(closeTriggerRight, 0.8, {
+    //     x: "+=100px", y: "+=100px", delay: 0.2, ease: Power4.easeOut
+    //   }, "preClose");
 
-    //CLOSE TIMELINE
-    tlClose.add("close")
-      .to(menuTop, 0.2, {
-        backgroundColor: "rgb(225,92,50)", ease: Power4.easeInOut, onComplete: function () {
-          logo.css('z-index', '26');
-          closeTrigger.css('z-index', '5');
-          openTrigger.css('visibility', 'visible');
-        }
-      }, "close")
-      .to(menuMiddle, 0.2, {
-        zIndex: 1,
-        backgroundColor: "rgb(225,92,50)", ease: Power4.easeInOut
-      }, "close")
-      .to(menuBottom, 0.2, {
-        zIndex: 1,
-        backgroundColor: "rgb(225,92,50)", ease: Power4.easeInOut
-      }, "close")
-      .to(menu, 0.6, {
-        zIndex: 1,
-        y: 20, opacity: 0, ease: Power4.easeOut, onComplete: function () {
-          menu.css('visibility', 'hidden');
-        }
-      }, "close")
-      .to(logo, 0.8, {
-        scale: 1, opacity: 1, ease: Power4.easeInOut
-      }, "close")
-      .to(menuTop, 0.8, {
-        y: "-113%",
-        ease: Power4.easeInOut
-      }, "close")
-      .to(menuMiddle, 0.8, {
-        scaleY: 0,
-        ease: Power4.easeInOut
-      }, "close")
-      .to(menuBottom, 0.8, {
-        y: "23%",
-        ease: Power4.easeInOut,
-        onComplete: function () {
-          menuTop.css('background-color', '#ffffff');
-          menuMiddle.css('background-color', '#ffffff');
-          menuBottom.css('background-color', '#ffffff');
-        }
-      }, "close")
-      .to(closeTriggerLeft, 0.2, {
-        x: "+=100px", y: "-=100px", ease: Power4.easeIn
-      }, "close")
-      .to(closeTriggerRight, 0.2, {
-        x: "-=100px", y: "-=100px", delay: 0.1, ease: Power4.easeIn
-      }, "close")
-      .to(openTriggerTop, 1, {
-        x: "-=80px", y: "+=80px", delay: 0.2, ease: Power4.easeOut
-      }, "close")
-      .to(openTriggerMiddle, 1, {
-        x: "-=80px", y: "+=80px", ease: Power4.easeOut
-      }, "close")
-      .to(openTriggerBottom, 1, {
-        x: "-=80px", y: "+=80px", delay: 0.1, ease: Power4.easeOut
-      }, "close");
+    // //CLOSE TIMELINE
+    // tlClose.add("close")
+    //   .to(menuTop, 0.2, {
+    //     backgroundColor: "rgb(225,92,50)", ease: Power4.easeInOut, onComplete: function () {
+    //       logo.css('z-index', '26');
+    //       closeTrigger.css('z-index', '5');
+    //       openTrigger.css('visibility', 'visible');
+    //     }
+    //   }, "close")
+    //   .to(menuMiddle, 0.2, {
+    //     zIndex: 1,
+    //     backgroundColor: "rgb(225,92,50)", ease: Power4.easeInOut
+    //   }, "close")
+    //   .to(menuBottom, 0.2, {
+    //     zIndex: 1,
+    //     backgroundColor: "rgb(225,92,50)", ease: Power4.easeInOut
+    //   }, "close")
+    //   .to(menu, 0.6, {
+    //     zIndex: 1,
+    //     y: 20, opacity: 0, ease: Power4.easeOut, onComplete: function () {
+    //       menu.css('visibility', 'hidden');
+    //     }
+    //   }, "close")
+    //   .to(logo, 0.8, {
+    //     scale: 1, opacity: 1, ease: Power4.easeInOut
+    //   }, "close")
+    //   .to(menuTop, 0.8, {
+    //     y: "-113%",
+    //     ease: Power4.easeInOut
+    //   }, "close")
+    //   .to(menuMiddle, 0.8, {
+    //     scaleY: 0,
+    //     ease: Power4.easeInOut
+    //   }, "close")
+    //   .to(menuBottom, 0.8, {
+    //     y: "23%",
+    //     ease: Power4.easeInOut,
+    //     onComplete: function () {
+    //       menuTop.css('background-color', '#ffffff');
+    //       menuMiddle.css('background-color', '#ffffff');
+    //       menuBottom.css('background-color', '#ffffff');
+    //     }
+    //   }, "close")
+    //   .to(closeTriggerLeft, 0.2, {
+    //     x: "+=100px", y: "-=100px", ease: Power4.easeIn
+    //   }, "close")
+    //   .to(closeTriggerRight, 0.2, {
+    //     x: "-=100px", y: "-=100px", delay: 0.1, ease: Power4.easeIn
+    //   }, "close")
+    //   .to(openTriggerTop, 1, {
+    //     x: "-=80px", y: "+=80px", delay: 0.2, ease: Power4.easeOut
+    //   }, "close")
+    //   .to(openTriggerMiddle, 1, {
+    //     x: "-=80px", y: "+=80px", ease: Power4.easeOut
+    //   }, "close")
+    //   .to(openTriggerBottom, 1, {
+    //     x: "-=80px", y: "+=80px", delay: 0.1, ease: Power4.easeOut
+    //   }, "close");
 
-    //EVENTS
-    openTrigger.on('click', function () {
-      if (tlOpen.progress() < 1) {
-        $('.btn').hide;
-        // document.addEventListener("touchmove", preventBehavior, {passive: false});
-        tlOpen.play();
+    // //EVENTS
+    // openTrigger.on('click', function () {
+    //   if (tlOpen.progress() < 1) {
+    //     $('.btn').hide;
+    //     // document.addEventListener("touchmove", preventBehavior, {passive: false});
+    //     tlOpen.play();
         
-        // preventBehavior
-      } else {
-        // document.addEventListener("touchmove", preventBehavior, {passive: false});
-        tlOpen.restart();
-      }
-    });
+    //     // preventBehavior
+    //   } else {
+    //     // document.addEventListener("touchmove", preventBehavior, {passive: false});
+    //     tlOpen.restart();
+    //   }
+    // });
 
-    closeTrigger.on('click', function () {
-      if (tlClose.progress() < 1) {
-        // document.removeEventListener("touchmove", preventBehavior1)
-        tlClose.play();
-      } else {
-        // document.removeEventListener("touchmove", preventBehavior1)
-        tlClose.restart();
-      }
-    });
-    closeTrigger1.on('click', function () {
-      if (tlClose.progress() < 1) {
-        // document.removeEventListener("touchmove", preventBehavior1)
-        tlClose.play();
-      } else { 
-        // document.removeEventListener("touchmove", preventBehavior1)
-        tlClose.restart();
-      }
-    });
+    // closeTrigger.on('click', function () {
+    //   if (tlClose.progress() < 1) {
+    //     // document.removeEventListener("touchmove", preventBehavior1)
+    //     tlClose.play();
+    //   } else {
+    //     // document.removeEventListener("touchmove", preventBehavior1)
+    //     tlClose.restart();
+    //   }
+    // });
+    // closeTrigger1.on('click', function () {
+    //   if (tlClose.progress() < 1) {
+    //     // document.removeEventListener("touchmove", preventBehavior1)
+    //     tlClose.play();
+    //   } else { 
+    //     // document.removeEventListener("touchmove", preventBehavior1)
+    //     tlClose.restart();
+    //   }
+    // });
 
     
 
