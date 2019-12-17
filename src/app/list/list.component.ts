6b7f1831d4355a7655ed3ece131e129920eb29f1
MaxLuu29 <datluu829@gmail.com>
2019-12-16 23:49:00 -0500

    image uploaded in add spot

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -10,52 +10,51 @@ declare let $: any;
 })
 
 export class ListComponent implements OnInit {
-  private electric: boolean = false;
-  private covered: boolean = false;
-  public nearByData: any = []
+  private electric = false;
+  private covered = false;
+  public nearByData: any = [];
   public reader;
-  constructor(private _router: Router) { }
-
-
-
-
-
-
-
+  constructor(private ListCompRouter: Router) { }
 
   submit() {
-    alert("T")
     const url = '/api/location/spot';
-    const title = $('#Email').val(); //title
-    const price = $('#Password').val(); //price
-    const img = $('#img').val(); //img
+    const title = $('#Title').val(); // title
+    const address = $('#Address').val(); // address
+    const price = $('#Price').val(); // price
+    const img = (document.getElementById('imageUpload') as HTMLInputElement).files[0]; // spot image
 
-    var userName = localStorage.getItem('username');
-    var userID = localStorage.getItem('email');
- 
-    var decodedJwtData;
-    if (localStorage.getItem('token') != null){
-    var token = localStorage.getItem('token');
-    let jwtData = token.split('.')[1]
-    let decodedJwtJsonData = window.atob(jwtData)
-    decodedJwtData = JSON.parse(decodedJwtJsonData)
-    } else{
-      this._router.navigate(['/unauthorized']);
+    let formData = new FormData();
+
+    const userName = localStorage.getItem('username');
+    const userID = localStorage.getItem('email');
+    let token: string;
+    let decodedJwtData;
+
+    if (localStorage.getItem('token') != null) {
+      token = localStorage.getItem('token');
+      const jwtData = token.split('.')[1];
+      const decodedJwtJsonData = window.atob(jwtData);
+      decodedJwtData = JSON.parse(decodedJwtJsonData);
+    } else {
+      this.ListCompRouter.navigate(['/unauthorized']);
     }
 
-    var today = new Date();
-    var dd = String(today.getDate()).padStart(2, '0');
-    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-    var yyyy = today.getFullYear();
+    const today = new Date();
+    const dd = String(today.getDate()).padStart(2, '0');
+    const mm = String(today.getMonth() + 1).padStart(2, '0'); // January is 0!
+    const yyyy = today.getFullYear();
 
-    let date= mm + '/' + dd + '/' + yyyy;
-    // var blobFile = $('#imgID')[0].files[0];
+    const date = mm + '/' + dd + '/' + yyyy;
+    // let blobFile = $('#imgID')[0].files[0];
     // alert(JSON.stringify($('#imgID')[0].files[0]))
-    // var reader = new FileReader();
-    // formData.append("fileToUpload", blobFile);
- 
+    // let reader = new FileReader();
 
-alert(this.reader)
+    formData.append('address', address);
+    formData.append('price', price);
+    formData.append('title', title);
+    formData.append('spotImage', img);
+
+    alert(this.reader);
     const data = {
       title: title,
       price: price,
@@ -67,90 +66,74 @@ alert(this.reader)
       // electric: this.electric,
       // covered: this.covered
     };
-    
-   
 
-//  alert(JSON.stringify(data))
-    
+    //  alert(JSON.stringify(data))
 
     const req = {
-      method: 'put',
+      method: 'post',
       headers: {
-        'content-type': 'application/x-www-form-urlencode',
         authorization: `Bearer ${token}`
       },
-      body: JSON.stringify(data)
+      body: formData
     };
 
-  
-    var that = this;
-
-    
+    const that = this;
 
     fetch(url, req)
-      .then(function () {
-        that._router.navigate(['/postlist']);
+      .then(() => {
+        that.ListCompRouter.navigate(['/postlist']);
       })
-      .catch(function(){
-        that._router.navigate(['/unauthorized']);
+      .catch(() => {
+        that.ListCompRouter.navigate(['/unauthorized']);
       });
-    
   }
 
-   readURL(input) {
-     alert(input)
+  readURL(input) {
+    alert(input);
     if (input.files && input.files[0]) {
-        var reader = new FileReader();
+      const reader = new FileReader();
 
-        reader.onload = function(e) {
-            $('#imagePreview').css('background-image', 'url('+e.target.result +')');
-            $('#imagePreview').hide();
-            $('#imagePreview').fadeIn(650);
-        }
-        reader.readAsDataURL(input.files[0]);
-        this.reader = reader;
-        alert(reader)
+      reader.onload = (e) => {
+        // $('#imagePreview').css('background-image', 'url(' + e.target.result + ')');
+        $('#imagePreview').hide();
+        $('#imagePreview').fadeIn(650);
+      };
+      reader.readAsDataURL(input.files[0]);
+      this.reader = reader;
+      alert(reader);
     }
-}
-
-  
+  }
 
   ngOnInit() {
     $('.container')
       .animate({ top: 0 })
       .delay(500);
 
-      $('#checkboxOne').click(()=> {
-        if(!this.electric){
-          // alert("electric");
-          this.electric = true;
-        } else {
-          this.electric = false;
-        }
+    $('#checkboxOne').click(() => {
+      if (!this.electric) {
+        // alert("electric");
+        this.electric = true;
+      } else {
+        this.electric = false;
+      }
     });
 
-    $('#checkboxTwo').click(()=> {
-      if(!this.covered){
+    $('#checkboxTwo').click(() => {
+      if (!this.covered) {
         // alert("covered");
         this.covered = true;
       } else {
         this.covered = false;
       }
-  });
-
-  
-$("#imageUpload").change(()=> {
-    this.readURL($("#imageUpload")[0]);
-    alert("changed")
-});
-
- 
+    });
 
 
-}
+    $('#imageUpload').change(() => {
+      this.readURL($('#imageUpload')[0]);
+      alert('changed');
+    });
 
-
-  
+  }
 }
 
 
--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -21,7 +21,7 @@ export class ModalComponent implements OnInit {
       if (input.files && input.files[0]) {
           var reader = new FileReader();
           reader.onload = function(e) {
-              $('#imagePreview').css('background-image', 'url('+e.target.result +')');
+              // $('#imagePreview').css('background-image', 'url('+e.target.result +')');
               $('#imagePreview').hide();
               $('#imagePreview').fadeIn(650);
           }
